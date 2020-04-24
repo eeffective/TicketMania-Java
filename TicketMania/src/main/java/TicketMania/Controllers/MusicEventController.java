b2dfd7dd791638ab0463c7590bc940c34a8c34ac
Efaliso <efalisotamerat@gmail.com>
2020-04-24 17:27:13 +0200

    musicevent rest api implemented
    - getall
    - getbyid
    - create
    - musicevent repo
    - musicevent controller
    - musicevent service

--- TicketMania/src/main/java/TicketMania/Controllers/MusicEventController.java
+++ TicketMania/src/main/java/TicketMania/Controllers/MusicEventController.java
@@ -1,11 +1,48 @@
 package TicketMania.Controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import TicketMania.Entities.MusicEvent;
+import TicketMania.Services.MusicEventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 @CrossOrigin("http://localhost:4200")
 @RequestMapping(path = "/musicevents")
 public class MusicEventController {
+
+    @Autowired
+    private final MusicEventService musicEventService;
+
+    public MusicEventController(MusicEventService musicEventService) {
+        this.musicEventService = musicEventService;
+    }
+
+    @GetMapping
+    public Collection<MusicEvent> getAll() {
+        return this.musicEventService.getAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<MusicEvent> get(@PathVariable Long id) {
+        try {
+            MusicEvent musicEvent = musicEventService.getById(id);
+            return new ResponseEntity<MusicEvent>(musicEvent, HttpStatus.OK);
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<MusicEvent> add(@RequestBody MusicEvent musicEvent) {
+        try {
+            musicEventService.create(musicEvent);
+            return new ResponseEntity<MusicEvent>(musicEvent, HttpStatus.OK);
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
 }
--- TicketMania/src/main/java/TicketMania/Services/MusicEventService.java
+++ TicketMania/src/main/java/TicketMania/Services/MusicEventService.java
@@ -1,7 +1,36 @@
 package TicketMania.Services;
 
+import TicketMania.Entities.MusicEvent;
+import TicketMania.Repositories.MusicEventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public class MusicEventService {
+
+    @Autowired
+    private final MusicEventRepository musicEventRepository;
+
+    public MusicEventService(MusicEventRepository musicEventRepository) {
+        this.musicEventRepository = musicEventRepository;
+    }
+
+    public void create(MusicEvent musicEvent){
+        try {
+            musicEventRepository.save(musicEvent);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    public Collection<MusicEvent> getAll(){
+        return musicEventRepository.findAll();
+    }
+
+    public MusicEvent getById(Long id){
+        return musicEventRepository.findById(id).get();
+    }
 }
